5016115815f32892ddabd44b13ebd99c8c7f8af7
Trishah08 <trishah5915@gmail.com>
2022-10-18 09:54:12 +0800

    User Class

---
+++ CarRentalSystem/src/carrentalsystem/User.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package carrentalsystem;
+
+/**
+ *
+ * @author trish
+ */
+public class User {
+    
+    protected String UserID;
+    public String Name;
+    protected String Password;
+    protected String Usertype;
+}
+
+    public User(String UserID, String Name, String Password, String Usertype) {
+    this.UserID = UserID;
+    this.Name = Name;
+    this.Password = Password;
+    this.Usertype = Usertype;
+    
+    
+  }
+
+UserID = new UserID();
+Name = new Name();
+
+    System.out.println(UserID);
